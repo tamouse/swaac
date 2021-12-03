d657714b44e939bd2b2b9faa9fbabb6eaa5646dc
Tamara Temple <tamouse@gmail.com>
2021-12-03 15:03:32 -0600

    [WIP]

--- publish-setup.el
+++ publish-setup.el
@@ -1,8 +1,8 @@
 (setq org-publish-project-alist
       '(("orgfiles"
-	 :base-directory "~/Sites/swaac/"
+	 :base-directory "~/Projects/Sites/swaac/"
 	 :base-extension "org"
-	 :publishing-directory "~/Sites/swaac-pub/"
+	 :publishing-directory "~/Projects/Sites/swaac-pub/"
 	 :publishing-function org-html-publish-to-html
 	 :headline-levels 3
 	 :section-numbers nil
